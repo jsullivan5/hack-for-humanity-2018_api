b151d408a35fe11f9747db173bb2840d863bf010
James Sullivan <james_sullivan@mckinsey.com>
2018-10-08 19:35:39 -0600

    Add logger to email route

--- api/routes/email.route.js
+++ api/routes/email.route.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const _ = require('lodash');
 
 const config = require('../util/config');
+const logger = require('../util/logger');
 
 const router = express.Router();
 
@@ -39,9 +40,9 @@ router.post('/', (req, res, next) => {
         return next(error);
       }
 
-      console.log('Message sent: %s', info.messageId);
+      logger.log('Message sent: %s', info.messageId);
       // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      logger.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
       // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
