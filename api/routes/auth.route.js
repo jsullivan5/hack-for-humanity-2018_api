2af9603a331e5cb2b0dca80e5bd580fcdc2550b2
James Sullivan <james_sullivan@mckinsey.com>
2018-10-08 22:14:26 -0600

    Fix linter errors auth route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
 const watsonAuthService = require('../services/watson.auth.service');
 
-router.get('/', async (req, res) => {
-   try {
+router.get('/', async (req, res, next) => {
+  try {
     const token = await watsonAuthService.authorize();
     res.status(200).send(token);
   } catch (error) {
